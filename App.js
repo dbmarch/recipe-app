dcec4025e12d8864750c72d97d46dc99632947b8
Dave March <dbmarch@gmail.com>
2019-09-22 15:59:06 -0400

    load the fonts

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font'
+import {AppLoading} from 'expo'
+
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans' : require ('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require ('./assets/fonts/OpenSans-Bold.ttf')
+  })
+  // .catch(err=>console.log(err))
+}
+
 
 export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false)
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading 
+        startAsync = {fetchFonts} 
+        onFinish = {()=>setDataLoaded(true)}
+        onError = {(err)=>console.log (err)}/>
+    )
+  }
+
   return (
-    <View>
+    <SafeAreaView>
       <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
