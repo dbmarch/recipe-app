4c7972a91227e7336fe13136a9192ba216860dd3
Dave March <dbmarch@gmail.com>
2019-09-30 23:47:30 -0400

    format the title

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity, ImageBackground} from 'react-native'
+import { setRecoveryProps } from 'expo/build/ErrorRecovery/ErrorRecovery'
 
 
 const MealItem = ({item, onSelectMeals}) => {
@@ -8,7 +9,11 @@ const MealItem = ({item, onSelectMeals}) => {
       <TouchableOpacity onPress = {onSelectMeals}>
         <View >
           <View style={{...styles.mealRow, ...styles.mealHeader}}>
-            <Text>{item.title}</Text>
+            <ImageBackground source = {{uri: item.imageUrl}} style={styles.bgImage}>
+              <View style= {styles.titleContainer}>
+                <Text style={styles.title} numberOfLines={1}>{item.title}</Text>
+              </View>
+            </ImageBackground>
           </View>
           <View style = {{...styles.mealRow , ...styles.mealDetails}}>
             <Text>{item.duration}m</Text>
@@ -25,20 +30,40 @@ const MealItem = ({item, onSelectMeals}) => {
 }
 
 const styles = StyleSheet.create({
+  bgImage: {
+    width: '100%',
+    heigth: '100%',
+    justifyContent: 'flex-end',
+    
+  },
   mealHeader: {
-    height: '90%'
+    height: '85%'
   },
   mealDetails: {
     paddingHorizontal: 10,
     justifyContent: 'space-between',
+    height: '15%',
+    overflow: 'hidden'
   },
   mealItem: {
     height: 200,
     width: '100%',
-    backroundColor: '#ccc'
+    backroundColor: '#f5f5f5',
+    borderRadius: 10,
   },
   mealRow: {
     flexDirection: 'row',
+  },
+  titleContainer: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padduingVertical: 5,
+    paddingHorizontal: 12,
+  },
+  title : {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    color: 'white',
+    textAlign: 'center'
   }
 })
 
